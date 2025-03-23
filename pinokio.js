9721898b544b8a75684bc78fc4613c198d354a2f
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-22 19:23:29 -0700

    update

--- pinokio.js
+++ pinokio.js
@@ -4,26 +4,89 @@ module.exports = {
   icon: "icon.png",
   description: "Style Aligned Image Generation via Shared Attention https://style-aligned-gen.github.io/",
   menu: async (kernel) => {
+    let installing = await kernel.running(__dirname, "install.js")
+    let installed = await kernel.exists(__dirname, "venv")
+    let running = await kernel.running(__dirname, "start.json")
+    if (installing) {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running) {
+        let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else {
+        return [{
+          default: true,
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
+      }
+    } else {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
     let installed = await kernel.exists(__dirname, "venv")
     if (installed) {
-      let session = await kernel.require(__dirname, "session.json")
       let running = await kernel.running(__dirname, "start.json")
       if (running) {
-        return [{
-          icon: "fa-solid fa-spin fa-circle-notch",
-          text: "Running"
-        }, {
-          icon: "fa-solid fa-terminal",
-          text: "Terminal",
-          href: "start.json",
-          target: "_blank"
-        }, {
-          default: true,
-          icon: "fa-solid fa-rocket",
-          text: "Open UI",
-          href: (session && session.url ? session.url : "http://127.0.0.1:7860"),
-          target: "_blank"
-        }]
+        let local = info.local("start.js")
+        if (local && local.url) {
+          return [{
+            icon: "fa-solid fa-terminal",
+            text: "Terminal",
+            href: "start.json",
+          }, {
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open UI",
+            href: local.url,
+          }]
+        } else {
+          return [{
+            icon: "fa-solid fa-terminal",
+            text: "Terminal",
+            href: "start.json",
+          }]
+        }
       } else {
         return [{
           default: true,
---
+++ update.js
@@ -0,0 +1,14 @@
+module.exports = {
+  run: [{
+    method: "shell.run",
+    params: {
+      message: "git pull"
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      path: "app",
+      message: "git pull"
+    }
+  }]
+}
