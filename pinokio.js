50f69c9d2ce084ed88375c935a1d477deba68cd2
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-22 19:28:22 -0700

    update

--- pinokio.js
+++ pinokio.js
@@ -7,6 +7,7 @@ module.exports = {
     let installing = await kernel.running(__dirname, "install.js")
     let installed = await kernel.exists(__dirname, "venv")
     let running = await kernel.running(__dirname, "start.json")
+    let updating = await kernel.running(__dirname, "update.js")
     if (installing) {
       return [{
         default: true,
@@ -36,6 +37,13 @@ module.exports = {
             href: "start.json",
           }]
         }
+      } else if (updating) {
+        return [{
+          default: true,
+          icon: "fa-solid fa-plug",
+          text: "Updating",
+          href: "update.js",
+        }]
       } else {
         return [{
           default: true,
